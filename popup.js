d84430cb57446549daf7e42b911b93f794986ce6
Hirbod03 <shoss2@uwo.ca>
2024-07-03 13:43:55 -0400

    added styling and images for buttons and header

--- popup.js
+++ popup.js
@@ -3,40 +3,47 @@ let startTime;
 let elapsedTime = 0;
 let running = false;
 
-document.getElementById('startStop').addEventListener('click', function() {
+const startStopButton = document.getElementById('startStop');
+const startStopImage = document.getElementById('startStopImage');
+const timerDisplay = document.getElementById('timer');
+const resetButton = document.getElementById('reset');
+
+startStopButton.addEventListener('click', function() {
   if (running) {
     clearInterval(timer);
     running = false;
-    this.textContent = 'Start';
+    startStopImage.src = 'icons/buttons/start.png';
   } else {
     startTime = Date.now() - elapsedTime;
-    timer = setInterval(updateTime, 1000);
+    timer = setInterval(updateTime, 10);  // Update every 10 milliseconds
     running = true;
-    this.textContent = 'Stop';
+    startStopImage.src = 'icons/buttons/pause.png';
   }
 });
 
-document.getElementById('reset').addEventListener('click', function() {
+resetButton.addEventListener('click', function() {
   clearInterval(timer);
   running = false;
   elapsedTime = 0;
-  document.getElementById('timer').textContent = '00:00:00';
-  document.getElementById('startStop').textContent = 'Start';
+  timerDisplay.textContent = '00:00:00.000';
+  startStopImage.src = 'icons/buttons/start.png';
 });
 
 function updateTime() {
   elapsedTime = Date.now() - startTime;
-  document.getElementById('timer').textContent = timeToString(elapsedTime);
+  timerDisplay.textContent = timeToString(elapsedTime);
 }
 
 function timeToString(time) {
   let hours = Math.floor(time / (1000 * 60 * 60));
   let minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((time % (1000 * 60)) / 1000);
+  let milliseconds = Math.floor((time % 1000));
 
   let formattedHours = hours.toString().padStart(2, '0');
   let formattedMinutes = minutes.toString().padStart(2, '0');
   let formattedSeconds = seconds.toString().padStart(2, '0');
+  let formattedMilliseconds = milliseconds.toString().padStart(3, '0');
 
-  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}.${formattedMilliseconds}`;
 }
